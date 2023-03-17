63b9b145ea92365db2ba48b71d7871a788bcde5f
peterannh <124418875+peterannh@users.noreply.github.com>
2023-03-17 19:15:47 +0900

    Created JS file

---
+++ script.js
@@ -0,0 +1,118 @@
+// Feature 1 - Current date and time
+let now = new Date();
+function formatDate(date) {
+  let day = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+
+  let month = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let currentDay = day[now.getDay()];
+  let currentMonth = month[now.getMonth()];
+  let currentDayOfMonth = now.getDate();
+  let currentTime = `${hours}:${minutes}`;
+  let formattedDate = `${currentDay}, ${currentMonth} ${currentDayOfMonth} ${currentTime}`;
+
+  let currentDateTime = document.getElementById("current-date-time");
+  currentDateTime.innerHTML = formattedDate;
+}
+
+formatDate(now);
+
+// Feature 2 - Search function that updates the city name, temperature and secondary information
+function displayWeatherInfo(response) {
+  console.log(response);
+  let cityTemp = Math.round(response.data.main.temp);
+  let currentTempValue = document.querySelector("#current-temp-value");
+  currentTempValue.innerHTML = cityTemp;
+
+  let searchedCityName = response.data.name;
+  let searchedCityElement = document.querySelector("#searched-city");
+  searchedCityElement.innerHTML = searchedCityName;
+
+  let weatherDescription = response.data.weather[0].main;
+  let weatherDescriptionElement = document.querySelector(
+    "#weather-description"
+  );
+  weatherDescriptionElement.innerHTML = weatherDescription;
+
+  let windMeasurement = Math.round(response.data.wind.speed);
+  let windMeasurementElement = document.querySelector("#wind-measurement");
+  windMeasurementElement.innerHTML = ` ${windMeasurement} km/h`;
+}
+
+function defaultCitySearch(city) {
+  let units = "metric";
+  let apiKey = "58a6775f97527351bf6c6966e209be39";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
+  axios.get(apiURL).then(displayWeatherInfo);
+  console.log(apiURL);
+}
+
+function userCitySearch(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  defaultCitySearch(city);
+}
+let citySearchForm = document.querySelector("#city-search");
+citySearchForm.addEventListener("submit", userCitySearch);
+
+defaultCitySearch("Tokyo");
+
+// Feature 3 - Current Location Button
+
+function currentLocationSearch(position) {
+  let apiKey = "58a6775f97527351bf6c6966e209be39";
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let units = "metric";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(apiURL).then(displayWeatherInfo);
+}
+
+function getCurrentLocation(event) {
+  navigator.geolocation.getCurrentPosition(currentLocationSearch);
+}
+
+let currentLocationBtn = document.getElementById("current-location-btn");
+currentLocationBtn.addEventListener("click", getCurrentLocation);
+
+// Feature 4 - Switch between celcius and farenheit
+
+function changeToFahrenheit(event) {
+  event.preventDefault();
+  let currentFarenheitTemp = document.querySelector("#current-temp-value");
+  currentFarenheitTemp.innerHTML = 66;
+}
+
+function changeToCelcius(event) {
+  event.preventDefault();
+  let curentCelciusTemp = document.querySelector("#current-temp-value");
+  curentCelciusTemp.innerHTML = 20;
+}
+
+let fahrenheitTemp = document.querySelector("#fahrenheit-temp-link");
+fahrenheitTemp.addEventListener("click", changeToFahrenheit);
+
+let celciusTemp = document.querySelector("#celcius-temp-link");
+celciusTemp.addEventListener("click", changeToCelcius);
